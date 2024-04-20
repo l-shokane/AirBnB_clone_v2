96dc51b656343c890059903aeed91f8e368ac6cc
masedie <masedie@masedies-MacBook-Air.local>
2024-04-20 23:28:07 +0200

    3. MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,16 @@
+--A script that prepares a MySQL server for the project
+
+-- Creates a database if it doesnt exist.
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Creates user if it doesnt exist.
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grants all priviledges on hbnb_dev_db to hbnb_dev.
+GRANT ALL PRIVILEDGES ON hbnb_dev_db.*TO 'hbnb_dev'@'localhost'.;
+
+-- Grants SELECT priviledges on perfomance_schema to hbnb_dev.
+GRANT SELECT ON perfomance_schema.*TO 'hbnb_dev'@'localhost'.;
+
+-- Flush all priviledges
+FLUSH PRIVILEDGES;
