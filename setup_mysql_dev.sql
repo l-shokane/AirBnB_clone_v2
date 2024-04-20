927b61437654b8326fff4c24c453c8ec073f0ce0
masedie <masedie@masedies-MacBook-Air.local>
2024-04-20 23:30:25 +0200

    3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,10 +7,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grants all priviledges on hbnb_dev_db to hbnb_dev.
-GRANT ALL PRIVILEDGES ON hbnb_dev_db.*TO 'hbnb_dev'@'localhost'.;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grants SELECT priviledges on perfomance_schema to hbnb_dev.
-GRANT SELECT ON perfomance_schema.*TO 'hbnb_dev'@'localhost'.;
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush all priviledges
 FLUSH PRIVILEDGES;
