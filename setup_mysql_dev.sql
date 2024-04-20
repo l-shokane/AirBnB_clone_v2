60e267b395d5d504402100a00d563210cd0a7273
masedie <masedie@masedies-MacBook-Air.local>
2024-04-21 00:14:37 +0200

    Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,29 @@
 --A script that prepares a MySQL server for the project
 
+--Connects to MySQL server.
+db = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='your_root_password'
+)
+--Creates a cursor object to execute queries.
+
 -- Creates a database if it doesnt exist.
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_dev_db")
 
 -- Creates user if it doesnt exist.
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'")
 
 -- Grants all privileges on hbnb_dev_db to hbnb_dev.
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'")
 
 -- Grants SELECT privileges on perfomance_schema to hbnb_dev.
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
+cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'")
 
 -- Flush all privileges
-FLUSH PRIVILEGES;
+cursor.execute("FLUSH PRIVILEGES")
+
+--Close the database and cursor.
+cursor.close()
+db.close()
