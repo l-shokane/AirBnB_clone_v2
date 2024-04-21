ce358d8f01c487ae92ecae727e7625c12041a430
masedie <masedie@masedies-MacBook-Air.local>
2024-04-21 02:36:26 +0200

    I want to make you smile

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""This module instantiates an object of class FileStorage
+    if environment variable 'HBNB_TYPE_STORAGE' is set
+    to 'db' import DBStorage ELSE Create an 
+    instance of FileStorage"""
 
+from os import getenv
 
-storage = FileStorage()
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
