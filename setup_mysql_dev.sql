3bd7d2248ed941e1ac8beaed92f73d1068bc516c
Masedie <masedie@Masedies-MacBook-Air.local>
2024-05-31 15:41:34 +0200

    Changes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,16 +1,16 @@
---A script that prepares a MySQL server for the project
+-- A script that prepares a MySQL server for the project
 
--- Creates a database if it doesnt exist.
+-- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Creates user if it doesnt exist.
+-- Create the user if it does not exist and set the password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grants all privileges on hbnb_dev_db to hbnb_dev.
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant all privileges on the new database to the user
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
--- Grants SELECT privileges on perfomance_schema to hbnb_dev.
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grant select privileges on the performance_schema to the user
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
--- Flush all privileges
+-- Apply the changes
 FLUSH PRIVILEGES;
