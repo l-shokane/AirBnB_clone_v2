99940f5446a60cb5378d3c28f6251c9ad9f1e649
Masedie <masedie@Masedies-MacBook-Air.local>
2024-05-31 15:48:37 +0200

    Changes

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,16 @@
---Script that prepares a MySQL server for the project:
+-- Script that prepares a MySQL server for the project:
 
---Creates a database if it doesn't exist.
+-- Creates a database if it doesn't exist.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
---Creates a user if it does not exist.
+-- Creates a user if it does not exist.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
---Grants all privileges from hbnb_test_db to hbnb_test.
+-- Grants all privileges from hbnb_test_db to hbnb_test.
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
---Grants SELECT privileges to hbnb_test from perfomance_schema.
+-- Grants SELECT privileges to hbnb_test from perfomance_schema.
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
---Flush privileges.
+-- Flush privileges.
 FLUSH PRIVILEGES;
