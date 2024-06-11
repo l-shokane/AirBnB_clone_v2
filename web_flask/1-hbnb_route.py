bb899ea2588253311b59ec344e4ffd72f3635ade
Masedie <masedie@Masedies-MacBook-Air.local>
2024-06-11 15:23:08 +0200

    Add

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,19 +11,25 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes=False
 
-app.route ('/')
-def home ():
+
+app.route('/')
+
+
+def home():
     """
     Displays 'Hello HBNB!'
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb')
-def hbnb ():
+
+
+def hbnb():
     """
     hbnb: displays 'HBNB'
     """
     return "HBNB"
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port = 5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
